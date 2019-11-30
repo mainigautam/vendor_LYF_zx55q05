8b6872a0f0d93a6160d24346ee3533e596ff25b1
Gautam <gautammaini9@gmail.com>
2019-11-30 19:01:34 +0000

    Boot Water-7

--- msm8916-common/Android.mk
+++ msm8916-common/Android.mk
@@ -17,7 +17,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter mobee01a mirage01a,$(TARGET_DEVICE)),)
+ifneq ($(filter zx55q05,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libloc_api_v02
